437ee16f865ce8662f4939754c0685880bc05996
Aitor Ibarguren <aitor.ibarguren.s@gmail.com>
2026-03-25 09:54:51 +0100

    1 initial implementation (#2)
    * Added msgs package
    
    * Added controller package
    
    * Added preliminary README
    
    * Added workflows
    
    * Remove Git link
    
    * Added controller code
    
    * Updated actions
    
    * Updated actions II
    
    * Updated actions III
    
    * Updated actions IV
    
    * Updated actions V
    
    * Updated actions VI
    
    * CI tests
    
    * CI tests II
    
    * CI tests III
    
    * CI tests IV
    
    * CI tests V
    
    * CI tests VI
    
    * Jazzy CI set
    
    * Fixed active axis management in callback
    
    * Updated README (preliminary)
    
    ---------
    
    Co-authored-by: 109053 <109053@TRI.LAN@WKM0737L.TRI.LAN>

---
+++ README.md
@@ -0,0 +1,77 @@
+# Adaptive Admittance Controller
+
+<p>
+  <a href="https://github.com/aitor-ibarguren/adaptive_admittance_controller/actions/workflows/ros2_jazzy_ci.yml">
+    <img src="https://github.com/aitor-ibarguren/adaptive_admittance_controller/actions/workflows/ros2_jazzy_ci.yml/badge.svg" alt="Build">
+  </a>
+</p>
+
+The `admittance_controller/AdaptiveAdmittanceController` is a chainable ROS2 controller designed to implement admittance control based on the wrench received from a force/torque sensor. This controller extendens the `admittance_controller/AdmittanceController` controller allowing the modification of its multiple parameters via topic with some additional features for a smooth admittance parameter modification.
+
+## Features
+
+- Chainable ROS2 controller designed to be placed as the last controller (e.g. after a *joint trajectory controller*). 
+- Accepts admittance parameters (stiffness, damping, mass, wrench command, active axes, and compliance frame) as well as tool parameters (mass and center-of-gravity) through topics.
+- Includes a low-pass filter to smooth the wrench values received from the force/torque sensor. 
+- The admittance control law calculates a twist value, which is internally computed to generate joint positions using **KDL** to generate the Jacobian matrix and **Eigen** to calculate the pseude-inverse using *SVD*.
+- Allows an **open-loop** mode in which the previously commanded joint positions are used instead of the joint positions from the state interfaces, avoiding the injection of hardware feedback latency and transport delays into the command generation loop.
+- The controller includes the option to **enable a feedback topic** the the admittance parameters and wrnech and twist values are published.
+
+## Configuration
+
+The next lines show a snippet of the *YAML* file defining the configuration of the `admittance_controller/AdaptiveAdmittanceController` controller for a UR16e robot:
+
+```yaml
+controller_manager:
+  ros__parameters:
+    use_sim_time: true
+    update_rate: 100  # Hz
+
+    ur_adaptive_admittance_controller:
+      type: admittance_controller/AdaptiveAdmittanceController
+
+ur_adaptive_admittance_controller:
+  ros__parameters:
+    is_chainable: true
+    joints:
+      - shoulder_pan_joint
+      - shoulder_lift_joint
+      - elbow_joint
+      - wrist_1_joint
+      - wrist_2_joint
+      - wrist_3_joint
+    state_interfaces:
+      - position
+      - velocity
+    command_interfaces:
+      - position
+    chainable_command_interfaces:
+      - position
+    open_loop: true
+    feedback_active: false
+    kinematics:
+      world_link: world
+      base_link: base_link
+      tip_link: tool0
+    ft_sensor:
+      name: ur_tcp_fts_sensor
+      frame_id: ur_tool0
+      filter_alpha: 0.025
+      remove_bias: true
+    tool:
+      mass: 2.0
+      CoG: [0.0, 0.0, 0.050]
+    admittance_control:
+      compliance_frame:
+        translation: [0.0, 0.0, 0.0]
+        rotation: [0.0, 0.0, 0.0, 1.0]
+      stiffness: [5000.0, 5000.0, 500.0, 5000.0, 5000.0, 5000.0]
+      damping: [50.0, 50.0, 50.0, 50.0, 50.0, 50.0]
+      mass: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+      wrench_command: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+      active_axes: [true, true, true, true, true, true]
+```
+
+## License
+
+The *adaptive_admittance_controller* repository has an Apache 2.0 license, as found in the [LICENSE](LICENSE) file.
---
+++ adaptive_admittance_controller/include/adaptive_admittance_controller/admittance_control_law_impl.hpp
@@ -0,0 +1,119 @@
+// Copyright (c) 2026 Aitor Ibarguren
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ADMITTANCE_CONTROLLER__ADMITTANCE_CONTROL_LAW_IMPL_HPP_
+#define ADMITTANCE_CONTROLLER__ADMITTANCE_CONTROL_LAW_IMPL_HPP_
+
+namespace admittance_controller
+{
+struct AdmittanceParams
+{
+  // Control params
+  Eigen::VectorXd stiffness;
+  Eigen::VectorXd damping;
+  Eigen::VectorXd mass;
+  Eigen::VectorXd wrench_command;
+
+  // Axis
+  std::vector<bool> active_axes;
+};
+
+class AdmittanceControlLaw
+{
+public:
+  AdmittanceControlLaw(const AdmittanceParams & admitance_params)
+  {
+    // Stiffness
+    K_.setZero();
+    K_.diagonal() = admitance_params.stiffness;
+    // Damping
+    D_.setZero();
+    D_.diagonal() = admitance_params.damping;
+    // Mass
+    M_.setZero();
+    M_.diagonal() = admitance_params.mass;
+    // Mass inverse
+    M_inv_ = M_.inverse();
+    // Wrench command
+    wrench_command_ = admitance_params.wrench_command;
+    // Active axes
+    active_axes_.setZero();
+    for (size_t i = 0; i < 6; i++) active_axes_(i, i) = admitance_params.active_axes[i] ? 1.0 : 0.0;
+
+    X_ = Eigen::VectorXd::Zero(6);
+    admittance_acceleration_ = Eigen::VectorXd::Zero(6);
+    admittance_velocity_ = Eigen::VectorXd::Zero(6);
+  }
+
+  void update_admittance_parameters(const AdmittanceParams & admitance_params)
+  {
+    // Stiffness
+    K_.setZero();
+    K_.diagonal() = admitance_params.stiffness;
+    // Damping
+    D_.setZero();
+    D_.diagonal() = admitance_params.damping;
+    // Mass
+    M_.setZero();
+    M_.diagonal() = admitance_params.mass;
+    // Mass inverse
+    M_inv_ = M_.inverse();
+    // Wrench command
+    wrench_command_ = admitance_params.wrench_command;
+    // Active axes
+    active_axes_.setZero();
+    for (size_t i = 0; i < 6; i++) active_axes_(i, i) = admitance_params.active_axes[i] ? 1.0 : 0.0;
+  }
+
+  Eigen::VectorXd update(
+    const Eigen::Isometry3d & base_H_compliance_frame_ref,
+    const Eigen::Isometry3d & base_H_compliance_frame_current, const Eigen::VectorXd & wrench,
+    double dt)
+  {
+    // Calculate X
+    Eigen::Isometry3d diff =
+      base_H_compliance_frame_ref.inverse() * base_H_compliance_frame_current;
+    X_.head<3>() = diff.translation();
+    Eigen::AngleAxisd aa(diff.rotation());
+    X_.tail<3>() = aa.angle() * aa.axis();
+
+    // Wrench
+    Eigen::VectorXd wrench_error = active_axes_ * (wrench - wrench_command_);
+
+    // Acceleration
+    admittance_acceleration_ = M_inv_ * (wrench_error - D_ * admittance_velocity_ - K_ * X_);
+    // Velocity
+    admittance_velocity_ = admittance_velocity_ + admittance_acceleration_ * dt;
+
+    // Return the admittance velocity (twist)
+    return admittance_velocity_;
+  }
+
+// private:
+  // Admittance control params
+  Eigen::VectorXd X_;
+  Eigen::Matrix<double, 6, 6> K_;
+  Eigen::Matrix<double, 6, 6> D_;
+  Eigen::Matrix<double, 6, 6> M_;
+  Eigen::Matrix<double, 6, 6> M_inv_;
+  Eigen::VectorXd wrench_command_;
+  Eigen::Matrix<double, 6, 6> active_axes_;
+
+  Eigen::VectorXd admittance_velocity_;
+  Eigen::VectorXd admittance_acceleration_;
+};
+
+}  // namespace admittance_controller
+
+#endif  // ADMITTANCE_CONTROLLER__ADMITTANCE_CONTROL_LAW_IMPL_HPP_
